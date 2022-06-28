2c85db56a605141ad7bb9040f0b905745414f966
Marcela Ixchel Gomez Wuotto <ixchelgo@hotmail.com>
2022-06-28 13:26:16 -0500

    redeploy

--- app/__init__.py
+++ app/__init__.py
@@ -8,59 +8,66 @@ from playhouse.shortcuts import model_to_dict
 load_dotenv()
 app = Flask(__name__)
 
-mydb =
-MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+mydb = MySQLDatabase(
+    os.getenv("MYSQL_DATABASE"),
     user=os.getenv("MYSQL_USER"),
     password=os.getenv("MYSQL_PASSWORD"),
     host=os.getenv("MYSQL_HOST"),
-    port=3306
+    port=3306,
 )
-print(mydb)
+
 
 class TimelinePost(Model):
     name = CharField()
     email = CharField()
     content = TextField()
-    created_at = DateTimeField(default = datetime.datetime.now)
+    created_at = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         database = mydb
 
+
 mydb.connect()
 mydb.create_tables([TimelinePost])
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html', title="Home", url=os.getenv("URL"))
+    return render_template("index.html", title="Home", url=os.getenv("URL"))
 
-@app.route('/professionalinfo')
+
+@app.route("/professionalinfo")
 def professionInfo():
-    return render_template('prof.html', title="Education/Experience", url=os.getenv("URL"))
+    return render_template(
+        "prof.html", title="Education/Experience", url=os.getenv("URL")
+    )
 
-@app.route('/hobbies')
+
+@app.route("/hobbies")
 def portfolio():
-    return render_template('hobbies.html', title="Hobbies", url=os.getenv("URL"))
+    return render_template("hobbies.html", title="Hobbies", url=os.getenv("URL"))
 
-@app.route('/api/timeline_post', methods=['POST'])
+
+@app.route("/api/timeline_post", methods=["POST"])
 def post_time_line_post():
-    name = request.form['name']
-    email = request.form['email']
-    content = request.form['content']
+    name = request.form["name"]
+    email = request.form["email"]
+    content = request.form["content"]
     timeline_post = TimelinePost.create(name=name, email=email, content=content)
 
     return model_to_dict(timeline_post)
 
-@app.route('/api/timeline_post', methods=['GET'])
+
+@app.route("/api/timeline_post", methods=["GET"])
 def get_time_line_post():
-    return{
-        'timeline_posts': [
+    return {
+        "timeline_posts": [
             model_to_dict(p)
-            for p in 
-TimelinePost.select().order_by(TimelinePost.created_at.desc())
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
         ]
     }
 
-@app.route('/timeline')
-def timeline():
-    return render_template('timeline.html', title="Timeline")
 
+@app.route("/timeline")
+def timeline():
+    return render_template("timeline.html", title="Timeline")
